dea0d127acf3eb94e16dea9cd94539ee3efe5359
LUCAS-PC\lucas <gombk666@gmail.com>
2017-11-05 12:09:35 -0200

    20k quando sai de Rook

--- data/npc/scripts/The Oracle3.lua
+++ data/npc/scripts/The Oracle3.lua
@@ -21,7 +21,7 @@ local config = {
 			--equipment spellbook, wand of vortex, magician's robe, mage hat, studded legs, leather boots, scarf
 			{{2175, 1}, {2190, 1}, {8819, 1}, {8820, 1}, {2468, 1}, {2643, 1}, {2661, 1}},
 			--container rope, shovel, mana potion
-			{{2120, 1}, {2554, 1}, {7620, 1}}
+			{{2120, 1}, {2554, 1}, {7620, 5}, {2160,2}}
 		},
 
 		["druid"] = {
@@ -30,7 +30,7 @@ local config = {
 			--equipment spellbook, snakebite rod, magician's robe, mage hat, studded legs, leather boots scarf
 			{{2175, 1}, {2182, 1}, {8819, 1}, {8820, 1}, {2468, 1}, {2643, 1}, {2661, 1}},
 			--container rope, shovel, mana potion
-			{{2120, 1}, {2554, 1}, {7620, 1}}
+			{{2120, 1}, {2554, 1}, {7620, 5}, {2160,2}}
 		},
 
 		["paladin"] = {
@@ -39,7 +39,7 @@ local config = {
 			--equipment dwrven shield, 5 spear, ranger's cloak, ranger legs scarf, legion helmet
 			{{2525, 1}, {2389, 5}, {2660, 1}, {8923, 1}, {2643, 1}, {2661, 1}, {2480, 1}},
 			--container rope, shovel, health potion, bow, 50 arrow
-			{{2120, 1}, {2554, 1}, {7618, 1}, {2456, 1}, {2544, 50}}
+			{{2120, 1}, {2554, 1}, {7618, 1}, {2456, 1}, {2544, 50}, {2160,2}}
 		},
 
 		["knight"] = {
@@ -48,7 +48,7 @@ local config = {
 			--equipment dwarven shield, steel axe, brass armor, brass helmet, brass legs scarf
 			{{2525, 1}, {8601, 1}, {2465, 1}, {2460, 1}, {2478, 1}, {2643, 1}, {2661, 1}},
 			--container jagged sword, daramian mace, rope, shovel, health potion
-			{{8602, 1}, {2439, 1}, {2120, 1}, {2554, 1}, {7618, 1}}
+			{{8602, 1}, {2439, 1}, {2120, 1}, {2554, 1}, {7618, 1}, {2160,2}}
 		}
 	}
 }
